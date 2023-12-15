b73b8b67b71d6847dfc3fdd4f5570418ffe59da8
Vados84 <vadimkosaservice@gmail.com>
2023-12-16 00:10:11 +0200

    hw1-js

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+function getShippingMessage(country, price, deliveryFee) {
+	const totalPrice = price + deliveryFee
+	const result = `Shipping to ${country} will cost ${totalPrice} credits`
+	return result
+}
+
+getShippingMessage('Australia', 120, 50)
+console.log(getShippingMessage('Australia', 120, 50))
+getShippingMessage('Germany', 80, 20)
+console.log(getShippingMessage('Germany', 80, 20))
+getShippingMessage('Sweden', 100, 20)
+console.log(getShippingMessage('Sweden', 100, 20))
